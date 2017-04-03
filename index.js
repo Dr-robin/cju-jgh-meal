45df9721098e56410d0c7121aad6a1656b4ad1cf
robin <robin@makichan.moe>
2017-04-03 17:40:13 +0900

    의문의 여1 처치

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
+function filter(s) {
+	s = s.replace(/\(여,1\)/, '');
+	s = s.replace(/\(1여\)/, '');
+}
 app.get('/:y/:m/:d/:t', function(req, res) {
 	request(`http://jungang.jje.hs.kr/jungang-h/food/${req.params.y}/${req.params.m}/${req.params.d}/${req.params.t}`, function(err, data, body) {
 		if(!err) {
@@ -16,7 +20,7 @@ app.get('/:y/:m/:d/:t', function(req, res) {
 					return;
 				}
 				mealArr.push(e.replace(/&#x(.{4});/g, function(match) {
-					return String.fromCharCode(parseInt(match.substr(3, 4), 16));
+					return filter(String.fromCharCode(parseInt(match.substr(3, 4), 16)));
 				}));
 			});
 			res.json({cal: kcal, meal: mealArr});
